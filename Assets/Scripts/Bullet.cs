40e97b1a651e3178ae505ec24d693013346f2cf1
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-03-25 14:42:17 -0400

    added

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(Target), typeof(Speed), typeof(Collision))]
 public class Bullet : MonoBehaviour
 {
-    public Vector2 Direction;
-    public GameObject Shooter;
+	public Vector2 Direction;
+	public GameObject Shooter;
 
-    private void Update()
-    {
-        transform.Translate(Direction * Time.deltaTime * GetComponent<Speed>().value);
-        GetComponent<Target>().Tag = Shooter.GetComponent<Target>().Tag;
-    }
+	private void Update()
+	{
+		transform.Translate(Direction * Time.deltaTime * GetComponent<Speed>().value);
+		if (GetComponent<Target>() != null && Shooter != null && Shooter.GetComponent<Target>() != null)
+			GetComponent<Target>().Tag = Shooter.GetComponent<Target>().Tag;
+	}
 }
