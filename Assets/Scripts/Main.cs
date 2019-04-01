bccd3d98c483930482662c85c2f79ac38c90c3a9
diaosuyidsy <suyi.diao@vanderbilt.edu>
2019-04-01 00:02:43 -0400

    State HW finished

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,30 +4,34 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-	public int numEnemies = 30;
-	public int batch = 2;
-	private readonly EnemyManager _manager = new EnemyManager();
+    public int numEnemies = 30;
+    public int batch = 2;
+    private readonly EnemyManager _manager = new EnemyManager ();
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		StartCoroutine(GenerateEnemies(2f));
-	}
+    public void GameStart ()
+    {
+        StartCoroutine (GenerateEnemies (2f));
+    }
 
-	IEnumerator GenerateEnemies(float time)
-	{
-		while (true)
-		{
-			yield return new WaitForSeconds(time);
+    public void GameEnd ()
+    {
+        StopAllCoroutines ();
+    }
 
-			for (int i = 0; i < batch && _manager.Population < numEnemies; i++)
-			{
-				var enemy = _manager.Create();
-				var randomPosition = Camera.main.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
-				randomPosition.z = 0f;
-				enemy.transform.position = randomPosition;
-			}
-		}
-	}
+    IEnumerator GenerateEnemies (float time)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds (time);
+
+            for (int i = 0; i < batch && _manager.Population < numEnemies; i++)
+            {
+                var enemy = _manager.Create ();
+                var randomPosition = Camera.main.ViewportToWorldPoint (new Vector2 (Random.value, Random.value));
+                randomPosition.z = 0f;
+                enemy.transform.position = randomPosition;
+            }
+        }
+    }
 
 }
---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+    public GameObject GameStartButton;
+    private FSM<StateManager> _fsm;
+
+    private void Start ()
+    {
+        _fsm = new FSM<StateManager> (this);
+
+        _fsm.TransitionTo<Prestart> ();
+
+    }
+
+    public void ChangeBackgroundColor (Color color)
+    {
+        Camera.main.backgroundColor = color;
+    }
+
+    public void ShowGameStartButton ()
+    {
+        GameStartButton.SetActive (true);
+    }
+
+    public void StartGame ()
+    {
+        GetComponent<Main> ().GameStart ();
+        GameStartButton.SetActive (false);
+
+    }
+
+    public void EndGame ()
+    {
+        GetComponent<Main> ().GameEnd ();
+    }
+
+    public void EraseEnemies ()
+    {
+        var bullets = GameObject.FindGameObjectsWithTag ("Bullet");
+        var enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+        foreach (var b in bullets)
+        {
+            Destroy (b);
+        }
+        foreach (var e in enemies)
+        {
+            Destroy (e);
+        }
+    }
+
+    public bool ClickedRestart ()
+    {
+        if (Input.GetKeyDown (KeyCode.R))
+            return true;
+        return false;
+    }
+
+    public bool ClickedStartButton ()
+    {
+        if (Input.GetMouseButtonUp (0))
+        {
+            var pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+            if (Vector2.Distance (pos, GameStartButton.transform.position) < 2f) return true;
+        }
+        return false;
+    }
+
+    private void Update ()
+    {
+        _fsm.Update ();
+    }
+}
+
+public class GameState : FSM<StateManager>.State
+{
+
+}
+
+public class Prestart : GameState
+{
+    public override void OnEnter ()
+    {
+        Context.ChangeBackgroundColor (Color.yellow);
+        Context.ShowGameStartButton ();
+    }
+
+    public override void Update ()
+    {
+        if (Context.ClickedStartButton ())
+        {
+            TransitionTo<Playing> ();
+            return;
+        }
+    }
+}
+
+public class Playing : GameState
+{
+    private float time = 10f;
+    public override void OnEnter ()
+    {
+        Context.StartGame ();
+        Context.ChangeBackgroundColor (Color.white);
+        this.time = 10f;
+    }
+
+    public override void Update ()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0f) TransitionTo<Ended> ();
+    }
+}
+
+public class Ended : GameState
+{
+    public override void OnEnter ()
+    {
+        Context.EndGame ();
+        Context.ChangeBackgroundColor (Color.black);
+        Context.EraseEnemies ();
+    }
+
+    public override void Update ()
+    {
+        if (Context.ClickedRestart ())
+        {
+            TransitionTo<Prestart> ();
+        }
+    }
+}
